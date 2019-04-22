b36e44f980af3e2fc542f3bc97175d46471f3ea3
RobertChenKFC <robert.chen.kfc@gmail.com>
2019-04-22 18:58:13 +0800

    Completed VM translator

--- vm-to-asm/src/main.cpp
+++ vm-to-asm/src/main.cpp
@@ -18,7 +18,9 @@ enum class Segment {
   THAT,
   POINTER,
   TMP,
-  STATIC
+  STATIC,
+  VALUE,
+  FRAME
 };
 
 void UnaryOperation(std::fstream &outputFile, char operation) {
@@ -38,23 +40,25 @@ void BinaryOperation(std::fstream &outputFile, char operation) {
     outputFile << "M=D" << operation << "M" << std::endl;
 }
 
-void CompareOperation(std::fstream &outputFile, const std::string &operation, std::int16_t labelNum) {
+std::int16_t labelNum = 0;
+void CompareOperation(std::fstream &outputFile, const std::string &operation) {
   outputFile << "@SP" << std::endl
     << "AM=M-1" << std::endl
     << "D=M" << std::endl
     << "A=A-1" << std::endl
     << "D=M-D" << std::endl
-    << "@CMP_BEGIN" << labelNum << std::endl
+    << "@TRANSLATOR_CMP_BEGIN" << labelNum << std::endl
     << "D;" << operation << std::endl
     << "D=0" << std::endl
-    << "@CMP_END" << labelNum << std::endl
+    << "@TRANSLATOR_CMP_END" << labelNum << std::endl
     << "0;JMP" << std::endl
-    << "(CMP_BEGIN" << labelNum << ")" << std::endl
+    << "(TRANSLATOR_CMP_BEGIN" << labelNum << ")" << std::endl
     << "D=-1" << std::endl
-    << "(CMP_END" << labelNum << ")" << std::endl
+    << "(TRANSLATOR_CMP_END" << labelNum << ")" << std::endl
     << "@SP" << std::endl
     << "A=M-1" << std::endl
     << "M=D" << std::endl;
+  ++labelNum;
 }
 
 void Push(std::fstream &outputFile, Segment segment, const std::string &value) {
@@ -108,9 +112,17 @@ void Push(std::fstream &outputFile, Segment segment, const std::string &value) {
         << "@SP" << std::endl
         << "M=M+1" << std::endl;
       return;
+    case Segment::VALUE:
+      outputFile << "@" << value << std::endl
+        << "D=M" << std::endl
+        << "@SP" << std::endl
+        << "A=M" << std::endl
+        << "M=D" << std::endl
+        << "@SP" << std::endl
+        << "M=M+1" << std::endl;
+      return;
     default:
-      // TODO: include all cases
-      break;
+      return;
   }
 
   outputFile << "@" << segmentAddress << std::endl
@@ -161,9 +173,22 @@ void Pop(std::fstream &outputFile, Segment segment, const std::string &value) {
         << "@" << value << std::endl
         << "M=D" << std::endl;
       return;
+    case Segment::VALUE:
+      outputFile << "@SP" << std::endl
+        << "AM=M-1" << std::endl
+        << "D=M" << std::endl
+        << "@" << value << std::endl
+        << "M=D" << std::endl;
+      return;
+    case Segment::FRAME:
+      outputFile << "@TRANSLATOR_frame" << std::endl
+        << "AM=M-1" << std::endl
+        << "D=M" << std::endl
+        << "@" << value << std::endl
+        << "M=D" << std::endl;
+      return;
     default:
-      // TODO: include all cases
-      break;
+      return;
   }
 
   outputFile << "@" << value << std::endl
@@ -182,12 +207,56 @@ void Pop(std::fstream &outputFile, Segment segment, const std::string &value) {
     << "M=M-D" << std::endl;
 }
 
+std::int16_t returnNum = 0;
+void Call(std::fstream &outputFile, const std::string &label, std::int16_t nArgs) {
+  std::string returnLabel = "TRANSLATOR_RETURN" + std::to_string(returnNum++);
+  Push(outputFile, Segment::CONSTANT, returnLabel);
+  Push(outputFile, Segment::VALUE, "LCL");
+  Push(outputFile, Segment::VALUE, "ARG");
+  Push(outputFile, Segment::VALUE, "THIS");
+  Push(outputFile, Segment::VALUE, "THAT");
+  outputFile << "@SP" << std::endl
+    << "D=M" << std::endl
+    << "@LCL" << std::endl
+    << "M=D" << std::endl
+    << "@" << 5 + nArgs << std::endl
+    << "D=D-A" << std::endl
+    << "@ARG" << std::endl
+    << "M=D" << std::endl
+    << "@" << label << std::endl
+    << "0;JMP" << std::endl
+    << "(" << returnLabel << ")" << std::endl;
+}
+
+void Return(std::fstream &outputFile) {
+  outputFile << "@LCL" << std::endl
+    << "D=M" << std::endl
+    << "@TRANSLATOR_frame" << std::endl
+    << "M=D" << std::endl
+    << "@5" << std::endl
+    << "A=D-A" << std::endl
+    << "D=M" << std::endl
+    << "@TRANSLATOR_retAddr" << std::endl
+    << "M=D" << std::endl;
+  Pop(outputFile, Segment::ARGUMENT, "0");
+  outputFile << "@ARG" << std::endl
+    << "D=M" << std::endl
+    << "@SP" << std::endl
+    << "M=D+1" << std::endl;
+  Pop(outputFile, Segment::FRAME, "THAT");
+  Pop(outputFile, Segment::FRAME, "THIS");
+  Pop(outputFile, Segment::FRAME, "ARG");
+  Pop(outputFile, Segment::FRAME, "LCL");
+  outputFile << "@TRANSLATOR_retAddr" << std::endl
+    << "A=M" << std::endl
+    << "0;JMP" << std::endl;
+}
+
 void Translate(std::fstream &outputFile, const std::string &inputFileName) {
   std::string inputClass = std::filesystem::path(inputFileName).filename().string() + ".";
   std::fstream inputFile(inputFileName, std::ios::in);
 
   std::string line;
-  std::int16_t labelNum = 0;
   std::size_t lineNum = 0;
   while (std::getline(inputFile, line)) {
     ++lineNum;
@@ -204,11 +273,11 @@ void Translate(std::fstream &outputFile, const std::string &inputFileName) {
     } else if (cmd == "neg") {
       UnaryOperation(outputFile, '-');
     } else if (cmd == "eq") {
-      CompareOperation(outputFile, "JEQ", labelNum++);
+      CompareOperation(outputFile, "JEQ");
     } else if (cmd == "gt") {
-      CompareOperation(outputFile, "JGT", labelNum++);
+      CompareOperation(outputFile, "JGT");
     } else if (cmd == "lt") {
-      CompareOperation(outputFile, "JLT", labelNum++);
+      CompareOperation(outputFile, "JLT");
     } else if (cmd == "and") {
       BinaryOperation(outputFile, '&');
     } else if (cmd == "or") {
@@ -261,6 +330,36 @@ void Translate(std::fstream &outputFile, const std::string &inputFileName) {
         throw std::runtime_error("Line " + std::to_string(lineNum) +
             ": Popping out of invalid segment \"" + cmd + "\"");
       }
+    } else if (cmd == "label") {
+      std::string label;
+      input >> label;
+      outputFile << "(" << label << ")" << std::endl;
+    } else if (cmd == "goto") {
+      std::string label;
+      input >> label;
+      outputFile << "@" << label << std::endl
+        << "0;JMP" << std::endl;
+    } else if (cmd == "if-goto") {
+      std::string label;
+      input >> label;
+      outputFile << "@SP" << std::endl
+        << "AM=M-1" << std::endl
+        << "D=M" << std::endl
+        << "@" << label << std::endl
+        << "D;JNE" << std::endl;
+    } else if (cmd == "function") {
+      std::string label;
+      std::int16_t nVars;
+      input >> label >> nVars;
+      outputFile << "(" << label << ")" << std::endl;
+      while (nVars--) Push(outputFile, Segment::CONSTANT, "0");
+    } else if (cmd == "call") {
+      std::string label;
+      std::int16_t nArgs;
+      input >> label >> nArgs;
+      Call(outputFile, label, nArgs);
+    } else if (cmd == "return") {
+      Return(outputFile);
     } else if (!std::all_of(cmd.begin(), cmd.end(),
           [](char c) { return std::isspace(static_cast<unsigned char>(c)); })) {
       throw std::runtime_error("Line " + std::to_string(lineNum) +
@@ -269,31 +368,48 @@ void Translate(std::fstream &outputFile, const std::string &inputFileName) {
   }
 }
 
-int main(int argc, char **argv) {
-  if (argc == 0) {
-    std::cout << "Usage: " << std::endl
-      << "    ./VMTranslator FILE.vm...    Translates all FILE.vm." << std::endl;
-  }
+void Bootstrap(std::fstream &outputFile) {
+  outputFile << "@256" << std::endl
+    << "D=A" << std::endl
+    << "@SP" << std::endl
+    << "M=D" << std::endl;
+  Call(outputFile, "Sys.init", 0);
+}
 
+int main(int argc, char **argv) {
+  // TODO: add options for no os and no bootstrap
   std::vector<std::string> inputFileNames;
   std::string outputFileName = "out.asm";
+  bool bootstrap = true;
   for (int i = 1; i < argc; ++i) {
-    if (std::strncmp(argv[i], "-o", 2) == 0) {
-      if (i + 1 >= argc) {
-        std::cout << "Missing filename after \"-o\"" << std::endl;
-        return 0;
+    std::string arg = argv[i];
+    if (arg.front() == '-') {
+      if (arg == "--help") {
+        std::cout << "Usage: " << std::endl
+          << "    ./VMTranslator FILE.vm...    Translates all FILE.vm." << std::endl
+          << "Options: " << std::endl
+          << "    --help                       Display this information." << std::endl
+          << "    --no-bootstrap               Translate vm-file without bootstrap code." << std::endl;
+      } else if (arg == "-o") {
+        if (i + 1 >= argc) {
+          std::cout << "Missing filename after \"-o\"" << std::endl;
+          return 0;
+        }
+        outputFileName = argv[i + 1];
+        ++i;
+      } else if (arg == "--no-bootstrap") {
+        bootstrap = false;
+      } else {
+        std::cout << "Unknown option \"" << arg << "\"" << std::endl;
       }
-      outputFileName = argv[i + 1];
-      ++i;
     } else {
       inputFileNames.push_back(argv[i]);
     }
   }
 
   std::fstream outputFile(outputFileName, std::ios::out);
+  if (bootstrap) Bootstrap(outputFile);
   for (const auto &inputFileName : inputFileNames) {
-    // TODO: make it so that it all outputs to the same file
-    // doing linking in the vm-translator
     try {
       Translate(outputFile, inputFileName);
     } catch (const std::exception &e) {
--- vm-to-asm/test/translator.sh
+++
@@ -1,9 +0,0 @@
-for projectDir in */; do
-    for category in ${projectDir}*; do
-        for testCase in ${category}/*; do
-            for fullFileName in ${testCase}/*.vm; do
-                ../build/VMTranslator fullFileName.vm
-            done
-        done
-    done
-done
